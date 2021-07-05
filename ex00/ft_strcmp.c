6298520799a59d825c7da2c00a44d04c64617bdc
Hugo Gicquel <hugogicquel@me.com>
2021-07-05 16:17:20 +0200

    fix

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -15,19 +15,25 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	char	c1;
-	char	c2;
-	int		i;
+	unsigned int	i;
 
 	i = 0;
-	while (1)
-	{
-		c1 = s1[i];
-		c2 = s2[i];
-		if (c1 == '\0')
-			return (0);
-		if (c1 != c2)
-			return (c1 - c2);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
-	}
+	return (s1[i] - s2[i]);
 }
+
+// int	main(void)
+// {
+// 	char s1[] = "Test1";
+// 	char s2[] = "OK";
+// 	char s3[] = "Same";
+// 	char s4[] = "Size";
+// 	char s5[] = "Shorter";
+// 	char s6[] = "ThanMyself";
+
+// 	printf("%d, %d\n", ft_strcmp(s1, s2), strcmp(s1, s2));
+// 	printf("%d, %d\n", ft_strcmp(s3, s4), strcmp(s3, s4));
+// 	printf("%d, %d\n", ft_strcmp(s5, s6), strcmp(s5, s6));
+// 	printf("%d, %d\n", ft_strcmp(s5, s5), strcmp(s5, s5));
+// }
